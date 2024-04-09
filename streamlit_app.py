9502f8f09950f1176d75dca7f056a18a549c92f6
Svrubio7 <svrubio7@gmail.com>
2024-04-09 17:23:58 +0200

    5th

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,30 +4,48 @@ import streamlit as st
 st.set_page_config(page_title="Your EV Online", page_icon=":electric_plug:", layout="wide")
 
 def main():
-    st.title("Welcome to Your EV Online!")
-
+    
+    #centering the title
+    st.markdown("<h1 style='text-align: center; color: black;'>Welcome to Your EV Online!</h1>", unsafe_allow_html=True)
+    #adding spacing
+    st.markdown("<h3 style='text-align: center; color: black;'> </h3>", unsafe_allow_html=True)
     # Container for company logo and introduction
     with st.container():
-        left_col, right_col = st.columns((1, 2))
+        l1, left_col, mid, right_col, r1 = st.columns(5)
         with left_col:
             st.image("elements/yeologo.jpg", width=300)
+        with mid:
+            st.empty()
         with right_col:
             st.markdown("""
             ## Explore the Future of Driving
             YEO offers top-tier electric vehicles designed for the modern era at the best prices. Experience sustainability, power, and cutting-edge technology with our range of EVs. Whether you're looking for city commuting or adventurous road trips, we've got you covered.
             """)
 
+
     st.markdown("---")  # Horizontal line to visually separate sections
 
     # Section for company values
     with st.container():
-        st.header("Why Choose YEO?")
-        st.markdown("""
-        **Sustainability:** Our EVs are designed with the planet in mind, ensuring you drive clean.
-        **Affordability:** Start saving with your new EV for the best prices in the whole industry.
-        **Support:** Contact us at any time! Our army of customer support agents will always respond.
-        """, unsafe_allow_html=True)
+        #centering the header
+        st.markdown("<h2 style='text-align: center; color: black;'>Why Choose YEO?</h2>", unsafe_allow_html=True)
+        #adding spacing
+        st.markdown("<h3 style='text-align: center; color: black;'> </h3>", unsafe_allow_html=True)
+        left, middle, right = st.columns(3) 
+        with left:
+            st.markdown("<h3 style='text-align: center; color: black;'>Sustainability</h3>", unsafe_allow_html=True)
+            st.markdown("<h4 style='text-align: center; color: black;'>Our EVs are designed with the planet in mind, ensuring you drive clean.</h4>", unsafe_allow_html=True)
+        
+        with right:
+            st.markdown("<h3 style='text-align: center; color: black;'>Affordability</h3>", unsafe_allow_html=True)
+            st.markdown("<h4 style='text-align: center; color: black;'>Start saving with your new EV for the best prices in the whole industry.</h4>", unsafe_allow_html=True)
 
+        with middle:
+            st.markdown("<h3 style='text-align: center; color: black;'>Support</h3>", unsafe_allow_html=True)
+            st.markdown("<h4 style='text-align: center; color: black;'>Contact us at any time! Our army of customer support agents will always respond.</h4>", unsafe_allow_html=True)
+
+            
+        
     st.markdown("---")  # Horizontal line
 
     # Contact form
